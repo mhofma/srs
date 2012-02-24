433a10101a36ef7a5a9e40f95ef64c78b0b253ac
michael.hofmann@profactor.at <michael.hofmann@profactor.at>
2012-02-24 10:45:01 +0100

    mod: added srs_or

---
+++ srs_or/src/or_state.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+#################################################################
+##\file
+#
+# \note
+#   Copyright (c) 2010 \n
+#   Fraunhofer Institute for Manufacturing Engineering
+#   and Automation (IPA) \n\n
+#
+#################################################################
+#
+# \note
+#   Project name: srs
+# \note
+#   ROS stack name: srs
+# \note
+#   ROS package name: srs_object_verification
+#
+# \author
+#   Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+#
+# \date Date of creation: Jan 2012
+#
+# \brief
+#   Implements generic states which can be used in multiple scenarios.
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer. \n
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution. \n
+#     - Neither the name of the Fraunhofer Institute for Manufacturing
+#       Engineering and Automation (IPA) nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################
+
+import roslib
+roslib.load_manifest('srs_or')
+import rospy
+import smach
+import smach_ros
+import actionlib
+import operator
+
+import sensor_msgs
+
+from srs_or.srv import *
+
+
+## Initialize state
+#
+# This state will initialize all hardware drivers.
+class DetectObject(smach.State):
+	def __init__(self):
+		smach.State.__init__(self,
+		outcomes=['succeeded', 'failed', 'not_completed', 'preempted'],
+		input_keys=['object_id','target_object_pointcloud'],
+		output_keys=['target_object_pose'])
+		self.or_client =  rospy.ServiceProxy('/or', or_request)
+
+	
+	def execute(self, userdata):
+		print userdata.object_id
+		print userdata.target_object_pointcloud.header
+		retVal = self.or_client(userdata.target_object_pointcloud, userdata.object_id)
+		#retVal = self.or_client(8, userdata.object_id)
+		#sensor_msgs/PointCloud2
+		print '4x4 Matrix values:'
+		for val in retVal.matrix:
+			print val
+		return 'succeeded'
+
+
+
---
+++ srs_or/src/test_or_state.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import roslib; roslib.load_manifest('srs_or')
+import rospy
+import smach
+import smach_ros
+import sys
+import rosbag
+from geometry_msgs.msg import Pose
+
+#import unittest
+
+from or_state import *
+
+class TestStates:
+	def __init__(self, *args):
+		rospy.init_node('test_states')
+	
+	def test_object_detection(self):
+		bag = rosbag.Bag('/home/mhofma/kinect.bag')
+		lastMsg = 0
+		for topic, msg, t in bag.read_messages(topics=['/camera/depth/points']):
+			lastMsg = msg
+		bag.close()
+		
+		
+		# create a SMACH state machine
+		SM = smach.StateMachine(outcomes=['overall_succeeded','overall_failed'])
+		SM.userdata.object_id = 1
+		SM.userdata.target_object_pointcloud = lastMsg
+	
+		# open the container
+		with SM:
+			smach.StateMachine.add('DETECT', DetectObject(),
+			transitions={'succeeded':'overall_succeeded', 'failed':'overall_failed', 'not_completed':'overall_failed', 'preempted':'overall_failed'})
+		try:
+			SM.execute()
+		except:
+			error_message = "Unexpected error:", sys.exc_info()[0]
+			#self.fail(error_message)
+
+# main
+if __name__ == '__main__':
+	test = TestStates()
+	test.test_object_detection()
